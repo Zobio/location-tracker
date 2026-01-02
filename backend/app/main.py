f91b3f346b7a505d02990c67ce82581cca37dfe6
Zobio <soshinito514@gmail.com>
2026-01-02 21:45:52 +0900

    chore: update CORS origins for Vercel deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,8 @@ app = FastAPI(
 origins = [
     "http://localhost:5173",
     "http://localhost:3000",
-    "https://*.vercel.app",
+    "https://location-tracker-two-vert.vercel.app",
+    "https://https://location-tracker-hw7e9impc-soshinito514gmailcoms-projects.vercel.app/"
     # 本番環境のフロントエンドURLをここに追加
     # 例: "https://location-tracker.vercel.app",
     # "https://*.vercel.app",  # Vercelのプレビューデプロイ用
